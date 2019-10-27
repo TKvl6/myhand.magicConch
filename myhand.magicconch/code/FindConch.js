98ba2767da861c1514cd2d1607a970e147db8282
lifaon <sm0514sm@gmail.com>
2019-10-28 01:52:43 +0900

    .

--- myhand.magicconch/code/FindConch.js
+++ myhand.magicconch/code/FindConch.js
@@ -4,20 +4,47 @@ var console = require('console')
 module.exports.function = function findConch(searchTerm) {
   const keysToSearchOn = ['title', 'artist', 'subtitle', 'albumName']
   let conchAudioFound = []
+  const probability = { 'yes': 0.35, 'again':0.40, 'no': 1 }
   var decisionVal = 0;
+  var topic = '';
+  var temp = Math.random();
 
+  console.log(temp)
+
+  if(temp <= probability.yes){
+    console.log("돼")
+    topic = 'yes'; //안돼
+  }
+  else if(temp <= probability.again){
+    console.log("다시")
+    topic = 'again'
+  }
+  else if(temp <= probability.no){
+    console.log("안돼")
+    topic = 'no'
+  }
   if (searchTerm) {
     console.log(searchTerm = encodeURI(searchTerm).replace(/%/gi, "").toLowerCase())
     for (let i = 0; i < searchTerm.length; i++) {
       decisionVal += searchTerm.charCodeAt(i)
     }
     decisionVal += doRandom(0, decisionVal)
-    decisionVal %= conchAudio.audioItems.length;
-  } 
-  else {
-    decisionVal = doRandom(0, conchAudio.audioItems.length)
+    decisionVal %= (conchAudio.audioItems.length - 2);
+    while((topic == 'yes' && decisionVal >= 2) || (topic == 'no' && decisionVal < 2)){
+      console.log(topic + " 인데 decisionVal : " + decisionVal + "이라서 다시 돌림")
+      decisionVal += doRandom(0, decisionVal)
+      decisionVal %= (conchAudio.audioItems.length - 2);
+    }
   }
-  
+  else {
+    if(topic == 'yes')
+      decisionVal = doRandom(0, 1)
+    else if(topic == 'no')
+      decisionVal = doRandom(3, 11-2) // 둘다, 굶어 제외
+    else if(topic == 'again')
+      decisionVal = 2
+  }
+  console.log("decisionVal : " + decisionVal)
   return [conchAudio.audioItems[decisionVal]]
 }
 
