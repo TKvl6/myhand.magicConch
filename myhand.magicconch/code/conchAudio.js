8445fbde039742c4033a13365a71e558c6cf63da
lifaon <sm0514sm@gmail.com>
2019-10-27 23:17:40 +0900

    conchAudio.js desc 속성 추가

--- myhand.magicconch/code/conchAudio.js
+++ myhand.magicconch/code/conchAudio.js
@@ -3,116 +3,19 @@ const audioItems = [
     id: 1,
     stream: [
       {
-        url: 'https://raw.githubusercontent.com/TKvl6/myhand.magicConch/master/myhand.magicconch/assets/audio/both.mp3',
+        url: 'https://raw.githubusercontent.com/TKvl6/myhand.magicConch/master/myhand.magicconch/assets/audio/sure.mp3',
         format: "mp3"
       }
     ],
-    title: '둘다',
+    title: '물론',
     artist: '마법의 소라고동님',
     subtitle: "Meow meow meow.",
     artist: "Radi-claw",
-    albumArtUrl: "https://storage.googleapis.com/bixby-audio-player-example/meows/cat.png"
+    albumArtUrl: "https://storage.googleapis.com/bixby-audio-player-example/meows/cat.png",
+    desc: "소라고동님이 안된다 하시네요! 절대 하면 안돼요..!",
   },
   {
     id: 2,
-    stream: [
-      {
-        url: 'https://raw.githubusercontent.com/TKvl6/myhand.magicConch/master/myhand.magicconch/assets/audio/do_______nt.mp3',
-        format: "mp3"
-      }
-    ],
-    title: '안    돼',
-    artist: '마법의 소라고동님',
-    subtitle: "Meow meow meow.",
-    artist: "Radi-claw",
-    albumArtUrl: "https://storage.googleapis.com/bixby-audio-player-example/meows/cat.png"
-  },
-  {
-    id: 3,
-    stream: [
-      {
-        url: 'https://raw.githubusercontent.com/TKvl6/myhand.magicConch/master/myhand.magicconch/assets/audio/dont.mp3',
-        format: "mp3"
-      }
-    ],
-    title: '안돼',
-    artist: '마법의 소라고동님',
-    subtitle: "Meow meow meow.",
-    artist: "Radi-claw",
-    albumArtUrl: "https://storage.googleapis.com/bixby-audio-player-example/meows/cat.png"
-  },
-  {
-    id: 4,
-    stream: [
-      {
-        url: 'https://raw.githubusercontent.com/TKvl6/myhand.magicConch/master/myhand.magicconch/assets/audio/dont_do_that_either.mp3',
-        format: "mp3"
-      }
-    ],
-    title: '그것도 안돼',
-    artist: '마법의 소라고동님',
-    subtitle: "Meow meow meow.",
-    artist: "Radi-claw",
-    albumArtUrl: "https://storage.googleapis.com/bixby-audio-player-example/meows/cat.png"
-  },
-  {
-    id: 5,
-    stream: [
-      {
-        url: 'https://raw.githubusercontent.com/TKvl6/myhand.magicConch/master/myhand.magicconch/assets/audio/little_long_dont.mp3',
-        format: "mp3"
-      }
-    ],
-    title: '약간 긴 안돼',
-    artist: '마법의 소라고동님',
-    subtitle: "Meow meow meow.",
-    artist: "Radi-claw",
-    albumArtUrl: "https://storage.googleapis.com/bixby-audio-player-example/meows/cat.png"
-  },
-  {
-    id: 6,
-    stream: [
-      {
-        url: 'https://raw.githubusercontent.com/TKvl6/myhand.magicConch/master/myhand.magicconch/assets/audio/question_again.mp3',
-        format: "mp3"
-      }
-    ],
-    title: '다시 한번 물어봐',
-    artist: '마법의 소라고동님',
-    subtitle: "Meow meow meow.",
-    artist: "Radi-claw",
-    albumArtUrl: "https://storage.googleapis.com/bixby-audio-player-example/meows/cat.png"
-  },
-  {
-    id: 7,
-    stream: [
-      {
-        url: 'https://raw.githubusercontent.com/TKvl6/myhand.magicConch/master/myhand.magicconch/assets/audio/short_dont.mp3',
-        format: "mp3"
-      }
-    ],
-    title: '짧은 안돼',
-    artist: '마법의 소라고동님',
-    subtitle: "Meow meow meow.",
-    artist: "Radi-claw",
-    albumArtUrl: "https://storage.googleapis.com/bixby-audio-player-example/meows/cat.png"
-  },
-  {
-    id: 8,
-    stream: [
-      {
-        url: 'https://raw.githubusercontent.com/TKvl6/myhand.magicConch/master/myhand.magicconch/assets/audio/short_low_dont.mp3',
-        format: "mp3"
-      }
-    ],
-    title: '짧고 낮은 안돼',
-    artist: '마법의 소라고동님',
-    subtitle: "Meow meow meow.",
-    artist: "Radi-claw",
-    albumArtUrl: "https://storage.googleapis.com/bixby-audio-player-example/meows/cat.png"
-  },
-  {
-    id: 9,
     stream: [
       {
         url: 'https://raw.githubusercontent.com/TKvl6/myhand.magicConch/master/myhand.magicconch/assets/audio/someday.mp3',
@@ -123,24 +26,116 @@ const audioItems = [
     artist: '마법의 소라고동님',
     subtitle: "Meow meow meow.",
     artist: "Radi-claw",
-    albumArtUrl: "https://storage.googleapis.com/bixby-audio-player-example/meows/cat.png"
+    albumArtUrl: "https://storage.googleapis.com/bixby-audio-player-example/meows/cat.png",
+    desc: "소라고동님이 안된다 하시네요! 절대 하면 안돼요..!",
   },
   {
-    id: 10,
+    id: 3,
     stream: [
       {
-        url: 'https://raw.githubusercontent.com/TKvl6/myhand.magicConch/master/myhand.magicconch/assets/audio/starve.mp3',
+        url: 'https://raw.githubusercontent.com/TKvl6/myhand.magicConch/master/myhand.magicconch/assets/audio/question_again.mp3',
         format: "mp3"
       }
     ],
-    title: '굶어',
+    title: '다시 한번 물어봐',
     artist: '마법의 소라고동님',
     subtitle: "Meow meow meow.",
     artist: "Radi-claw",
-    albumArtUrl: "https://storage.googleapis.com/bixby-audio-player-example/meows/cat.png"
+    albumArtUrl: "https://storage.googleapis.com/bixby-audio-player-example/meows/cat.png",
+    desc: "소라고동님이 안된다 하시네요! 절대 하면 안돼요..!",
   },
   {
-    id: 11,
+    id: 4,
+    stream: [
+      {
+        url: 'https://raw.githubusercontent.com/TKvl6/myhand.magicConch/master/myhand.magicconch/assets/audio/do_______nt.mp3',
+        format: "mp3"
+      }
+    ],
+    title: '안    돼',
+    artist: '마법의 소라고동님',
+    subtitle: "Meow meow meow.",
+    artist: "Radi-claw",
+    albumArtUrl: "https://storage.googleapis.com/bixby-audio-player-example/meows/cat.png",
+    desc: "소라고동님이 안된다 하시네요! 절대 하면 안돼요..!",
+  },
+  {
+    id: 5,
+    stream: [
+      {
+        url: 'https://raw.githubusercontent.com/TKvl6/myhand.magicConch/master/myhand.magicconch/assets/audio/dont.mp3',
+        format: "mp3"
+      }
+    ],
+    title: '안돼',
+    artist: '마법의 소라고동님',
+    subtitle: "Meow meow meow.",
+    artist: "Radi-claw",
+    albumArtUrl: "https://storage.googleapis.com/bixby-audio-player-example/meows/cat.png",
+    desc: "소라고동님이 안된다 하시네요! 절대 하면 안돼요..!",
+  },
+  {
+    id: 6,
+    stream: [
+      {
+        url: 'https://raw.githubusercontent.com/TKvl6/myhand.magicConch/master/myhand.magicconch/assets/audio/dont_do_that_either.mp3',
+        format: "mp3"
+      }
+    ],
+    title: '그것도 안돼',
+    artist: '마법의 소라고동님',
+    subtitle: "Meow meow meow.",
+    artist: "Radi-claw",
+    albumArtUrl: "https://storage.googleapis.com/bixby-audio-player-example/meows/cat.png",
+    desc: "소라고동님이 안된다 하시네요! 절대 하면 안돼요..!",
+  },
+  {
+    id: 7,
+    stream: [
+      {
+        url: 'https://raw.githubusercontent.com/TKvl6/myhand.magicConch/master/myhand.magicconch/assets/audio/little_long_dont.mp3',
+        format: "mp3"
+      }
+    ],
+    title: '약간 긴 안돼',
+    artist: '마법의 소라고동님',
+    subtitle: "Meow meow meow.",
+    artist: "Radi-claw",
+    albumArtUrl: "https://storage.googleapis.com/bixby-audio-player-example/meows/cat.png",
+    desc: "소라고동님이 안된다 하시네요! 절대 하면 안돼요..!",
+  },
+  {
+    id: 8,
+    stream: [
+      {
+        url: 'https://raw.githubusercontent.com/TKvl6/myhand.magicConch/master/myhand.magicconch/assets/audio/short_dont.mp3',
+        format: "mp3"
+      }
+    ],
+    title: '짧은 안돼',
+    artist: '마법의 소라고동님',
+    subtitle: "Meow meow meow.",
+    artist: "Radi-claw",
+    albumArtUrl: "https://storage.googleapis.com/bixby-audio-player-example/meows/cat.png",
+    desc: "소라고동님이 안된다 하시네요! 절대 하면 안돼요..!",
+  },
+  {
+    id: 9,
+    stream: [
+      {
+        url: 'https://raw.githubusercontent.com/TKvl6/myhand.magicConch/master/myhand.magicconch/assets/audio/short_low_dont.mp3',
+        format: "mp3"
+      }
+    ],
+    title: '짧고 낮은 안돼',
+    artist: '마법의 소라고동님',
+    subtitle: "Meow meow meow.",
+    artist: "Radi-claw",
+    albumArtUrl: "https://storage.googleapis.com/bixby-audio-player-example/meows/cat.png",
+    desc: "소라고동님이 안된다 하시네요! 절대 하면 안돼요..!",
+  },
+  {
+    id: 10,
     stream: [
       {
         url: 'https://raw.githubusercontent.com/TKvl6/myhand.magicConch/master/myhand.magicconch/assets/audio/stay_still.mp3',
@@ -151,22 +146,39 @@ const audioItems = [
     artist: '마법의 소라고동님',
     subtitle: "Meow meow meow.",
     artist: "Radi-claw",
-    albumArtUrl: "https://storage.googleapis.com/bixby-audio-player-example/meows/cat.png"
+    albumArtUrl: "https://storage.googleapis.com/bixby-audio-player-example/meows/cat.png",
+    desc: "소라고동님이 안된다 하시네요! 절대 하면 안돼요..!",
+  },
+  {
+    id: 11,
+    stream: [
+      {
+        url: 'https://raw.githubusercontent.com/TKvl6/myhand.magicConch/master/myhand.magicconch/assets/audio/both.mp3',
+        format: "mp3"
+      }
+    ],
+    title: '둘다',
+    artist: '마법의 소라고동님',
+    subtitle: "Meow meow meow.",
+    artist: "Radi-claw",
+    albumArtUrl: "https://storage.googleapis.com/bixby-audio-player-example/meows/cat.png",
+    desc: "소라고동님이 안된다 하시네요! 절대 하면 안돼요..!",
   },
   {
     id: 12,
     stream: [
       {
-        url: 'https://raw.githubusercontent.com/TKvl6/myhand.magicConch/master/myhand.magicconch/assets/audio/sure.mp3',
+        url: 'https://raw.githubusercontent.com/TKvl6/myhand.magicConch/master/myhand.magicconch/assets/audio/starve.mp3',
         format: "mp3"
       }
     ],
-    title: '물론',
+    title: '굶어',
     artist: '마법의 소라고동님',
     subtitle: "Meow meow meow.",
     artist: "Radi-claw",
-    albumArtUrl: "https://storage.googleapis.com/bixby-audio-player-example/meows/cat.png"
-  },
+    albumArtUrl: "https://storage.googleapis.com/bixby-audio-player-example/meows/cat.png",
+    desc: "소라고동님이 안된다 하시네요! 절대 하면 안돼요..!",
+  }
 ]
 
 exports.audioItems = audioItems
